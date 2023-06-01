055547038a36937b8e3222533b01b4a381708dd2
Rysitas <bayroncaceres17@gmail.com>
2023-05-31 22:46:19 -0500

    Css post produccion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,6 @@
         @yield('styles')
 
         <!-- Scripts -->
-        <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://use.fontawesome.com/61d0f4506d.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.3/umd/popper.min.js" ></script>
